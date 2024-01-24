ec7d848db407ced9caddf85c740e76b4e356f415
BiagioCaputo <scebiagio98@gmail.com>
2024-01-24 11:26:04 +0100

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log("Vue ok", Vue)
+
+const {createApp} = Vue;
+
+const app = createApp({
+    name:"Boolzapp",
+    data: () => ({
+    }),
+     
+    computed: {
+    },
+
+    methods: {
+
+        
+    }
+
+});
+
+app.mount('#root');
