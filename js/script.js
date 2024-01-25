4ef7561ea2fbb5734ef2880d72437b900def5b69
BiagioCaputo <scebiagio98@gmail.com>
2024-01-25 16:47:22 +0100

    Funzione ricerca utenti implementata, terminato quindi il Milestone 4

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const {createApp} = Vue;
 
 const app = createApp({
     name:"Boolzapp",
+
     data: () => ({
         data,
         user: data.user,
@@ -11,14 +12,22 @@ const app = createApp({
         activeId: 1,
         activeContact: {},
         newMessageText:"",
+        searcheduser:""
 
     }),
+
     computed: {
         //metodo per ricavare il contatto attualmente attivo nella lista
         currentContact() {
             return this.contacts.find(contact => contact.id === this.activeId);
-        }
+        },
+        
+        //metodo che mi restituisce un array di contatti contenenti la stringa di caratteri inserita dall'utente
+        searchedConversations() {
+            const searchedUser = this.searcheduser.toLowerCase();
 
+            return this.contacts.filter(contact => contact.name.toLowerCase().includes(searchedUser));
+        }
     },
 
     methods: {
@@ -38,7 +47,7 @@ const app = createApp({
             currentContact.messages.push(newAnswer);
         },
         
-        //funzione per aggiungere un messaggio alla chat attuale e ricevere una risposta automatica dopo 1 secondo
+        //funzione per aggiungere un messaggio alla chat attuale e riceve una risposta automatica dopo 1 secondo
         addMessage(currentContact){
             const newMessage = {
                 id: new Date().toISOString(),
