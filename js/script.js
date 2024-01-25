eadc0e547198acc22f14522c2092a0ece212fc2f
BiagioCaputo <scebiagio98@gmail.com>
2024-01-25 13:34:23 +0100

    reso dinamico il contatto con il quale l'utente sta interagendo

--- js/script.js
+++ js/script.js
@@ -8,19 +8,26 @@ const app = createApp({
         data,
         user: data.user,
         contacts: data.contacts,
-        
+        activeId: 1,
+        activeContact: {}
+
     }),
-     
     computed: {
+        //metodo per ricavare il contatto attualmente attivo nella lista
+        currentContact() {
+            return this.contacts.find(contact => contact.id === this.activeId);
+        }
+
     },
 
     methods: {
+        //funzione per impostare le immagini degli utenti
         getAvatarPic (avatar) {
             return `img/avatar${avatar}.jpg`;
-        }
+        },
         
     }
-
 });
+
 
 app.mount('#root');
